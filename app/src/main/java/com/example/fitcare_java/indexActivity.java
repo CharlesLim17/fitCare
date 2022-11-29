dfd5e455464cecfc23d0a63b7d27adf0180d2e5c
CharlesLim17 <choylim06@gmail.com>
2022-11-29 21:52:39 +0800

    update

--- app/src/main/java/com/example/fitcare_java/indexActivity.java
+++ app/src/main/java/com/example/fitcare_java/indexActivity.java
@@ -1,20 +1,54 @@
 package com.example.fitcare_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.fitcare_java.databinding.ActivityIndexBinding;
+
 public class indexActivity extends AppCompatActivity {
+
+    //view binding
+    ActivityIndexBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_index);
+        binding = ActivityIndexBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //replacing fragment
+        replaceFragment(new homeFragment());
 
         //hide action bar
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
 
+        //bottom navigation bar
+        binding.navBar.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new homeFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new settingsFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    //function for changing fragments
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
