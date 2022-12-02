c70f16ebb004963a24def83346873f4085a62240
CharlesLim17 <choylim06@gmail.com>
2022-12-03 00:19:37 +0800

    update

--- app/src/main/java/com/example/fitcare_java/homeFragment.java
+++ app/src/main/java/com/example/fitcare_java/homeFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class homeFragment extends Fragment {
@@ -18,6 +19,7 @@ public class homeFragment extends Fragment {
     TextView btnTracker;
     TextView btnWarmup;
     TextView btnReminder;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
