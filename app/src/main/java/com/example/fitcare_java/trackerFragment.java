4974f609ce06467dcead89c75551b1f8c13a42c9
CharlesLim17 <choylim06@gmail.com>
2022-12-12 22:09:00 +0800

    retrieved data workout Tracker

--- app/src/main/java/com/example/fitcare_java/trackerFragment.java
+++ app/src/main/java/com/example/fitcare_java/trackerFragment.java
@@ -2,9 +2,11 @@ package com.example.fitcare_java;
 
 import static android.app.Activity.RESULT_OK;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -14,8 +16,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -25,7 +36,15 @@ public class trackerFragment extends Fragment {
     //declaring variables
     ImageView btnBack, btnEdit;
     FloatingActionButton btnMic;
+    TextView txtRetrieveCurWeight, txtRetrievePrevWeight, txtRetrieveGoal, txtRetrieveVS, txtRetrieveToAchieveGoal;
 
+    //firebase
+    FirebaseUser user;
+    DatabaseReference databaseReference;
+    FirebaseAuth auth;
+    String uid;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -36,6 +55,20 @@ public class trackerFragment extends Fragment {
         btnBack = view.findViewById(R.id.btnBack);
         btnEdit = view.findViewById(R.id.btnEdit);
         btnMic = view.findViewById(R.id.btnMic);
+        txtRetrieveCurWeight = view.findViewById(R.id.txtRetrieveCurWeight);
+        txtRetrievePrevWeight = view.findViewById(R.id.txtRetrievePrevWeight);
+        txtRetrieveGoal = view.findViewById(R.id.txtRetrieveGoal);
+        txtRetrieveVS = view.findViewById(R.id.txtRetrieveVS);
+        txtRetrieveToAchieveGoal = view.findViewById(R.id.txtRetrieveToAchieveGoal);
+
+        //firebase instance
+        auth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference("User");
+        uid = user.getUid();
+
+        //retrieve data
+        readData();
 
         //back onclick
         btnBack.setOnClickListener(new View.OnClickListener() {
@@ -66,6 +99,80 @@ public class trackerFragment extends Fragment {
         });
 
         return view;
+    }
+
+    //function to retrieve data
+    private void readData(){
+        databaseReference.child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+
+                if (user !=null) {
+                    float curWeight = user.curWeight;
+                    float prevWeight = user.prevWeight;
+                    float goal = user.goal;
+
+                    txtRetrieveCurWeight.setText(curWeight + "kgs");
+                    txtRetrievePrevWeight.setText(prevWeight + "kgs");
+                    txtRetrieveGoal.setText(goal + "kgs");
+
+                    //Current weight vs Previous Weight
+                    float curVSprev;
+                    String descVS;
+                    if (curWeight > prevWeight) {
+                        curVSprev = curWeight - prevWeight;
+                        descVS = "- ";
+
+                        //retrieve 2 decimal places for curVsPrev
+                        @SuppressLint("DefaultLocale") String formattedString1 = String.format("%.02f", curVSprev);
+                        txtRetrieveVS.setText(descVS + formattedString1 + "kgs");
+                    }
+                    else if (curWeight < prevWeight){
+                        curVSprev = prevWeight - curWeight;
+                        descVS = "+ ";
+
+                        //retrieve 2 decimal places for curVsPrev
+                        @SuppressLint("DefaultLocale") String formattedString1 = String.format("%.02f", curVSprev);
+                        txtRetrieveVS.setText(descVS + formattedString1 + "kgs");
+                    }
+                    else{
+                        curVSprev = curWeight - prevWeight;
+                        descVS = "= ";
+
+                        //retrieve 2 decimal places for curVsPrev
+                        @SuppressLint("DefaultLocale") String formattedString1 = String.format("%.02f", curVSprev);
+                        txtRetrieveVS.setText(descVS + formattedString1 + "kgs");
+                    }
+
+                    //To achieve Goal
+                    float toAchieveGoal;
+                    if (goal > curWeight){
+                        toAchieveGoal = goal - curWeight;
+
+                        //retrieve 2 decimal places for to achieve goal
+                        @SuppressLint("DefaultLocale") String formattedString2 = String.format("%.02f", toAchieveGoal);
+                        txtRetrieveToAchieveGoal.setText("You need to gain "+formattedString2 + "kgs");
+                    }
+                    else if(goal < curWeight){
+                        toAchieveGoal = curWeight - goal;
+
+                        //retrieve 2 decimal places for to achieve goal
+                        @SuppressLint("DefaultLocale") String formattedString2 = String.format("%.02f", toAchieveGoal);
+                        txtRetrieveToAchieveGoal.setText("You need to lose " + formattedString2 + "kgs");
+                    }
+                    else {
+                        txtRetrieveToAchieveGoal.setText("Congratulations! You achieved your weight goal!");
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     //function for opening mic
