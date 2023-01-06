dc3c7389a617c097dda0210d17d0a0cd3c8f11d0
parparu <anotdejasper2@gmail.com>
2023-01-07 06:29:16 +0800

    Meal Plan Edit (Done) for Polishing

--- app/src/main/java/com/example/fitcare_java/mealPlanEditFragment.java
+++ app/src/main/java/com/example/fitcare_java/mealPlanEditFragment.java
@@ -168,10 +168,10 @@ public class mealPlanEditFragment extends Fragment {
             }
         };
 
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int year = getYear();
+        int month = getMonth();
+        month -= 1;
+        int day = getDay();
 
         int style = AlertDialog.THEME_HOLO_LIGHT;
 
