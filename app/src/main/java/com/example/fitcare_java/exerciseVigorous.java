4ca1aeafe5eb3a093eea9f5202884bbca6da3184
CharlesLim17 <choylim06@gmail.com>
2023-01-11 18:52:40 +0800

    Exercise Warmmp vids complete

--- app/src/main/java/com/example/fitcare_java/exerciseVigorous.java
+++ app/src/main/java/com/example/fitcare_java/exerciseVigorous.java
@@ -104,18 +104,28 @@ public class exerciseVigorous extends AppCompatActivity {
 
                 //checking if it is in fullscreen or not and setting specified tasks
                 if(!isFullScreen){
-                    bt_fullscreen.setImageDrawable(ContextCompat
-                            .getDrawable(getApplicationContext(), R.drawable.ic_baseline_fullscreen_exit));
-                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                    //change drawable icon
+                    bt_fullscreen.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_baseline_fullscreen_exit));
+
+                    //change layout to landscape and full screen
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
                     ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
                     playerView.setLayoutParams(layoutParams);
+
+                    // to hide status and navigation
                     playerView.setResizeMode(AspectRatioFrameLayout.RESIZE_MODE_FILL);
+                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
                 }
                 else{
-                    bt_fullscreen.setImageDrawable(ContextCompat
-                            .getDrawable(getApplicationContext(), R.drawable.ic_baseline_fullscreen));
+                    //change drawable icon
+                    bt_fullscreen.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_baseline_fullscreen));
+
+                    //change layout to original form
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
                     playerView.setLayoutParams(originalLayoutParams);
+
+                    //to show nav and status bar
+                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
                 }
                 isFullScreen = !isFullScreen;
 
