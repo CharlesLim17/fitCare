ebb6834d26da1bbd953c4069a0115b0dceb7ecc7
CharlesLim17 <choylim06@gmail.com>
2023-01-13 18:42:34 +0800

    about, and privacy rephrased

---
+++ app/src/main/java/com/example/fitcare_java/settingsUserEdit.java
@@ -0,0 +1,21 @@
+package com.example.fitcare_java;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class settingsUserEdit extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings_user_edit, container, false);
+    }
+}
